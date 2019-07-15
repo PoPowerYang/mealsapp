97da02c8868aa4e438c6c14eaa08d809f4d140ae
PoPowerYang <yangsunsunshine@gmail.com>
2019-07-14 21:27:22 -0400

    image added

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/meal_item.dart';
+import '../data/dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
-  // final String categoryID;
-  // final String categoryTitle;
-
-  // CategoryMealsScreen(this.categoryID, this.categoryTitle);
-
+  
   @override
   Widget build(BuildContext context) {
     final routeArgs = 
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryID = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryID);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -20,9 +22,15 @@ class CategoryMealsScreen extends StatelessWidget {
         ),
       body: ListView.builder(
           itemBuilder: (ctx, index) {
-
+            return MealItem(
+              title: categoryMeals[index].title,
+              affordability: categoryMeals[index].affordability,
+              complexity: categoryMeals[index].complexity,
+              duration: categoryMeals[index].duration,
+              imageURL: categoryMeals[index].imageUrl,
+            );
           }, 
-      // itemCount: ,
+      itemCount: categoryMeals.length,
       ),
     );
   }
